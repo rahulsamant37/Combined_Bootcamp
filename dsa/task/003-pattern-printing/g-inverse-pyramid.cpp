04923a99e5850b72d247b0445887a2ee5d3c8409
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-04-23 14:56:58 +0530

    feat: add f-pyramid.cpp for left-aligned pyramid pattern printing

---
+++ dsa/task/003-pattern-printing/g-inverse-pyramid.cpp
@@ -0,0 +1,48 @@
+/*
+Author: Rahul Samant
+Created: 2026-04-14 11:31:21
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main() {
+    setup_io();
+    int n;
+    cin >> n;
+    string str;
+    for (int i = n; i > 0; i--) {
+        str = string(i, '*');
+        cout << str << endl;
+    }
+    return 0;
+}
---
+++ dsa/task/003-pattern-printing/h-numbered-traingle.cpp
@@ -0,0 +1,51 @@
+/*
+Author: Rahul Samant
+Created: 2026-04-14 11:42:22
+*/
+
+#include <bits/stdc++.h>
+#include <print>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main() {
+    setup_io();
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        string str;
+        for (int j = 1; j <= i; j++) {
+            str+=to_string(i);
+        }
+        cout << str << endl;
+    }
+    return 0;
+}
