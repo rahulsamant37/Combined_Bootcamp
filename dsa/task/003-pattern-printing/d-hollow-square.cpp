82d678787357b13062d51c0f17ff391d5207df41
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-04-23 14:56:49 +0530

    feat: add c-find-maximum.cpp for printing rectangle of stars

---
+++ dsa/task/003-pattern-printing/d-hollow-square.cpp
@@ -0,0 +1,53 @@
+/*
+Author: Rahul Samant
+Created: 2026-04-10 03:07:09
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main() {
+    setup_io();
+    int n;
+    cin >> n;
+    if (n==1) {
+        cout << '*';
+        return 0;
+    }
+    string str(n, '*'), str_b = '*' + string(n-2, ' ') + '*';
+    cout << str << endl;
+    for(int i = 1; i<=n-2; i++){
+        cout << str_b << endl;
+    }
+    cout << str << endl;
+    return 0;
+}
