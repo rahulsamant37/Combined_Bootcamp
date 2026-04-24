17f4fc28e89382ac93b0ae8bee95c45113b17328
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-04-24 17:51:18 +0530

    feat: add d-check-prime for primality checking

---
+++ dsa/task/004-function/d-check-prime.cpp
@@ -0,0 +1,57 @@
+/*
+Author: Rahul Samant
+Created: 2026-04-24 09:40:45
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main() {
+    setup_io();
+    int n;
+    cin >> n;
+
+    if (n < 2) {
+        cout << "Not Prime";
+        return 0;
+    }
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            cout << "Not Prime";
+            return 0;
+        }
+    }
+
+    cout << "Prime";
+    return 0;
+}
