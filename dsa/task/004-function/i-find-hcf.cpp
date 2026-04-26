6996fee16af7b8e514e1697d15fad636c687de38
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-04-26 08:39:31 +0530

    feat: Add HCF (GCD) calculator using Euclidean algorithm

---
+++ dsa/task/004-function/i-find-hcf.cpp
@@ -0,0 +1,53 @@
+/*
+Author: Rahul Samant
+Created: 2026-04-26 08:32:58
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int findHCF(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    setup_io();
+    int n, m;
+    cin >> n >> m;
+    cout << findHCF(n, m) << endl;
+    return 0;
+}
