0facc653a7f080196e11ef5b8f033a462922a3d2
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-04-26 08:38:24 +0530

    feat: add factorial calculator with iterative implementation

---
+++ dsa/task/004-function/e-factorial.cpp
@@ -0,0 +1,52 @@
+/*
+Author: Rahul Samant
+Created: 2026-04-26 08:36:15
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+long long factorial(int n) {
+    long long result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+int main() {
+    setup_io();
+    int n;
+    cin >> n;
+    cout << factorial(n) << endl;
+    return 0;
+}
