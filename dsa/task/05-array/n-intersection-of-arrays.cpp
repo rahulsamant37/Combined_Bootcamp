bd5ee8457f4d9e71e0b6525099928df2d56ae4b9
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-05-17 19:09:16 +0530

    feat: add efficient intersection of two arrays implementation

---
+++ dsa/task/05-array/n-intersection-of-arrays.cpp
@@ -0,0 +1,80 @@
+/*
+Author: Rahul Samant
+Created: 2026-05-11 17:36:50
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    int N;
+    cin >> N;
+
+    vector<int> a(N);
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+    }
+
+    int M;
+    cin >> M;
+
+    unordered_map<int, int> freq;
+    for (int i = 0; i < M; i++) {
+        int x;
+        cin >> x;
+        freq[x]++;
+    }
+
+    bool first = true;
+
+    for (int x : a) {
+        if (freq[x] > 0) {
+            if (!first)
+                cout << " ";
+            cout << x;
+            first = false;
+            freq[x]--;
+        }
+    }
+
+    cout << '\n';
+}
+
+int main() {
+    setup_io();
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
