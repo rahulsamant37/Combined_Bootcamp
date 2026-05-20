8a1de5ef0382776d1dabf570680ae6fb45e93a98
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-05-20 22:07:25 +0530

    feat: add quadruplet count solution using 4 nested loops

---
+++ dsa/task/05-array/q-count-quadraplets.cpp
@@ -0,0 +1,63 @@
+/*
+Author: Rahul Samant
+Created: 2026-05-14 21:43:46
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main() {
+    setup_io();
+    int n;
+    long long x;
+    cin >> n >> x;
+
+    vector<long long> A(n);
+    for (int i = 0; i < n; i++) cin >> A[i];
+
+    long long ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            for (int k = j + 1; k < n; k++) {
+                for (int l = k + 1; l < n; l++) {
+                    if (A[i] - 2 * A[j] + 3 * A[k] - 4 * A[l] == x) {
+                        ans++;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << ans << '\n';
+    return 0;
+}
