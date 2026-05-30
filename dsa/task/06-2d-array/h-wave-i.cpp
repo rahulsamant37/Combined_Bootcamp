4e756db45f520c873bdb9ede9a208d37743f051b
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-05-30 14:40:50 +0530

    feat: add H-wave traversal for 2D array

---
+++ dsa/task/06-2d-array/h-wave-i.cpp
@@ -0,0 +1,65 @@
+/*
+Author: Rahul Samant
+Created: 2026-05-21 21:44:32
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main() {
+    setup_io();
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> a(n, vector<int>(m));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            // Left to right
+            for (int j = 0; j < m; j++) {
+                cout << a[i][j] << " ";
+            }
+        } else {
+            // Right to left
+            for (int j = m - 1; j >= 0; j--) {
+                cout << a[i][j] << " ";
+            }
+        }
+    }
+    return 0;
+}
