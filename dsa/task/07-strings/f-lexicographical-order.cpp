4f16400963543cb7f9d7792c6f3e0b437ffaf181
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-06-07 06:26:11 +0530

    feat: solve lexicographical order string comparison problem

---
+++ dsa/task/07-strings/f-lexicographical-order.cpp
@@ -0,0 +1,50 @@
+/*
+Author: Rahul Samant
+Created: 2026-05-31 07:41:25
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main() {
+    setup_io();
+    string A, B;
+    cin >> A >> B;
+    if (A < B) {
+        cout << "A";
+    } else if (A > B) {
+        cout << "B";
+    } else {
+        cout << "Equal";
+    }
+    return 0;
+}
