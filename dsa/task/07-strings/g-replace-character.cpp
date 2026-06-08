81c75e68abe220cf59de10f08132611538a87420
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-06-08 09:55:17 +0530

    feat: add character replacement solution

---
+++ dsa/task/07-strings/g-replace-character.cpp
@@ -0,0 +1,54 @@
+/*
+Author: Rahul Samant
+Created: 2026-06-01 09:46:06
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main() {
+    setup_io();
+    string s;
+    cin >> s;
+
+    char c1, c2;
+    cin >> c1 >> c2;
+
+    for (char &ch : s) {
+        if (ch == c1) {
+            ch = c2;
+        }
+    }
+
+    cout << s << '\n';
+    return 0;
+}
