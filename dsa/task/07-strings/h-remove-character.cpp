3ef16d745a05465160331ccbdb0ead35579e6814
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-06-09 21:19:28 +0530

    feat: Add character removal implementation for strings task

---
+++ dsa/task/07-strings/h-remove-character.cpp
@@ -0,0 +1,55 @@
+/*
+Author: Rahul Samant
+Created: 2026-06-02 07:15:10
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main() {
+    setup_io();
+    string s;
+    char x;
+
+    cin >> s;
+    cin >> x;
+
+    string result;
+    for (char c : s) {
+        if (c != x) {
+            result += c;
+        }
+    }
+
+    cout << result;
+    return 0;
+}
