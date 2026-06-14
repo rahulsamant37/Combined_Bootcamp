776aa8246edfd59140c664fd61ec80c47903611c
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-06-14 08:17:37 +0530

    feat: add solution for string reversal problem

---
+++ dsa/task/07-strings/l-reverse.cpp
@@ -0,0 +1,47 @@
+/*
+Author: Rahul Samant
+Created: 2026-06-06 07:04:28
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main() {
+    setup_io();
+    string s;
+    cin >> s;
+
+    reverse(s.begin(), s.end());
+
+    cout << s << endl;
+    return 0;
+}
