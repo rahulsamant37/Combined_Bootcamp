dc468a48ffed8f998d7af7be24e77f5cb833bc3d
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-06-21 18:44:21 +0530

    feat: add solution for push zeros to the end problem

---
+++ dsa/task/09-mixed-practice/b-push-zeros-to-the-end.cpp
@@ -0,0 +1,81 @@
+/*
+Author: Rahul Samant
+Created: 2026-06-21 18:39:18
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<long long> nonZero;
+    int zeroCount = 0;
+
+    for (int i = 0; i < n; i++) {
+        long long x;
+        cin >> x;
+
+        if (x == 0)
+            zeroCount++;
+        else
+            nonZero.push_back(x);
+    }
+
+    bool first = true;
+
+    for (long long x : nonZero) {
+        if (!first)
+            cout << ' ';
+        cout << x;
+        first = false;
+    }
+
+    for (int i = 0; i < zeroCount; i++) {
+        if (!first)
+            cout << ' ';
+        cout << 0;
+        first = false;
+    }
+    cout << '\n';
+}
+
+int main() {
+    setup_io();
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
