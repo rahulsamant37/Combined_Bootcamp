2c0a5692693cee3c79a3ac152bd6d42a8255ad09
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-06-28 22:36:44 +0530

    feat: add Sum of Digits II solution

---
+++ dsa/task/09-mixed-practice/e-sum-of-digits-ii.cpp
@@ -0,0 +1,50 @@
+/*
+Author: Rahul Samant
+Created: 2026-06-26 22:30:50
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main() {
+    setup_io();
+    string n;
+    cin >> n;
+
+    int sum = 0;
+    for (char c : n) {
+        sum += c - '0';
+    }
+
+    cout << sum << endl;
+    return 0;
+}
