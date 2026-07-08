5e1bdbbbfda91370487502f7d918704baaf3f075
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-07-08 22:41:23 +0530

    feat: Solve Interval Sum using arithmetic progression formula

---
+++ dsa/task/10-time-and-space-complexity/b-interval-sum.cpp
@@ -0,0 +1,53 @@
+/*
+Author: Rahul Samant
+Created: 2026-07-08 22:40:07
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main() {
+    setup_io();
+    int q;
+    cin >> q;
+
+    while (q--) {
+        long long l, r;
+        cin >> l >> r;
+
+        long long n = r - l + 1;
+        long long ans = n * (l + r) / 2;
+
+        cout << ans << '\n';
+    }
+    return 0;
+}
