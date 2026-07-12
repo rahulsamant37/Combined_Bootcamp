6bd8286a1c7ca8dc34f3a1bcebe0755f40353385
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-07-12 23:16:17 +0530

    feat: add efficient function to calculate number of divisors

---
+++ dsa/task/10-time-and-space-complexity/d-count-factors.cpp
@@ -0,0 +1,62 @@
+/*
+Author: Rahul Samant
+Created: 2026-07-12 23:04:59
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+ll numberOfDivisors(ll num) {
+    ll total = 1;
+    for (int i = 2; (ll)i * i <= num; i++) {
+        if (num % i == 0) {
+            int e = 0;
+            do {
+                e++;
+                num /= i;
+            } while (num % i == 0);
+            total *= e + 1;
+        }
+    }
+    if (num > 1) {
+        total *= 2;
+    }
+    return total;
+}
+
+int main() {
+    setup_io();
+    ll n;
+    cin >> n;
+    cout << numberOfDivisors(n);
+    return 0;
+}
