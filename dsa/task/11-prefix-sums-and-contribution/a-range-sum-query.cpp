b1178505729a8c151dd6148c30709be689036c54
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-07-27 23:13:24 +0530

    feat: implement prefix sum for efficient range sum queries

---
+++ dsa/task/11-prefix-sums-and-contribution/a-range-sum-query.cpp
@@ -0,0 +1,69 @@
+/*
+Author: Rahul Samant
+Created: 2026-07-27 23:11:39
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+template<typename T>
+void show_all(std::string_view rem, const T& v)
+{
+    std::cout << rem << ": ";
+    for (const auto& e : v)
+        std::cout << e << ' ';
+    std::cout << '\n';
+}
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main() {
+    setup_io();
+    int n;
+    cin >> n;
+
+    vector<long long> prefix(n + 1, 0);
+
+    for (int i = 1; i <= n; i++) {
+        long long x;
+        cin >> x;
+        prefix[i] = prefix[i - 1] + x;
+    }
+
+    int q;
+    cin >> q;
+
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+        cout << prefix[r] - prefix[l - 1] << "\n";
+    }
+    return 0;
+}
