acf9ac910fd675dba6a4d87c48207fccbec40716
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-07-31 22:34:53 +0530

    feat: Add prefix sum solution for vowel range quries

---
+++ dsa/task/11-prefix-sums-and-contribution/c-count-vowels.cpp
@@ -0,0 +1,74 @@
+/*
+Author: Rahul Samant
+Created: 2026-07-31 22:33:42
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+template<typename T>
+void show_all(std::string_view rem, const T& v)
+{
+    std::cout << rem << ": ";
+    for (const auto& e : v)
+        std::cout << e << ' ';
+    std::cout << '\n';
+}
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+bool isVowel(char c) {
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+}
+
+int main() {
+    setup_io();
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    vector<int> pref(n + 1, 0);
+
+    for (int i = 1; i <= n; i++) {
+        pref[i] = pref[i - 1] + (isVowel(s[i - 1]) ? 1 : 0);
+    }
+
+    int q;
+    cin >> q;
+
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+        cout << pref[r] - pref[l - 1] << '\n';
+    }
+    return 0;
+}
