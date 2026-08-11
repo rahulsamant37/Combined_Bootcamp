1d89fe697237e22eb1bfd0a976853b929387977e
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-08-11 19:38:50 +0530

    feat: add solution for sum of all subarrays separately

---
+++ dsa/task/11-prefix-sums-and-contribution/j-sum-of-all-subarrays-separately-hard.cpp
@@ -0,0 +1,64 @@
+/*
+Author: Rahul Samant
+Created: 2026-08-11 19:37:45
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+template<typename T>
+void show_all(std::string_view rem, const T& v)
+{
+    std::cout << rem << ": ";
+    for (const auto& e : v)
+        std::cout << e << ' ';
+    std::cout << '\n';
+}
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main() {
+    setup_io();
+    int n;
+    cin >> n;
+
+    vector<long long> a(n);
+    for (auto &x : a) cin >> x;
+
+    for (int L = 0; L < n; ++L) {
+        long long sum = 0;
+
+        for (int R = L; R < n; ++R) {
+            sum += a[R];
+            cout << sum << '\n';
+        }
+    }
+    return 0;
+}
